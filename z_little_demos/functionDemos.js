aa037af3cc91bdd302457f4f02bbad5a113c88b8
xch <158972928@qq.com>
2017-08-27 22:41:26 +0800

    添加个codevars的例子

--- z_little_demos/functionDemos.js
+++ z_little_demos/functionDemos.js
@@ -1,7 +1,11 @@
+//收集的是自己在codevars上的练习，重难点收集
 //1.对数组里的奇数排序，偶数位置不变
 function sortArray(array) {
   const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
   return array.map((x) => x % 2 ? odd.shift() : x);
 }
 
-//2.
+//2.返回字符串中的元音的（a,e,i,o,u）个数
+function getCount(str){
+  return (str.match(/[aeiou]/ig||[])).length;
+}
