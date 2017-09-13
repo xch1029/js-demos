dc0add9d06635a635965664fffde89afec5c1287
xch <158972928@qq.com>
2017-09-13 23:09:45 +0800

    jquery实现轮播图

---
+++ very_simple_slider/main.js
@@ -0,0 +1,61 @@
+$(function () {
+    var timer;
+    $('#checkbox').change(function(e){
+        console.log(e.target.checked);
+        if(!e.target.checked){
+            clearInterval(timer);
+            return
+        }
+        if(timer){
+            clearInterval(timer);
+        }
+        timer = setInterval(function () {
+            moveRight()
+        }, 1000);
+    });
+
+    var slideCount = $('#slider ul li').length;
+    var slideWidth = $('#slider ul li').width();
+    var slideHeight = $('#slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+
+    $('#slider').css({
+        width: slideWidth,
+        height: slideHeight
+    })
+
+    $('#slider ul').css({
+        width: sliderUlWidth,
+        marginLeft: -slideWidth
+    })
+
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft(){
+        $('#slider ul').animate({
+            left: +slideWidth
+        },200,function(){
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        })
+    }
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+
+})
